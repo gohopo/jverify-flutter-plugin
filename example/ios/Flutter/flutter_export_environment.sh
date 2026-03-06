0525f752352e0b50872c098127788f591b034df6
gohopo <gohopo@live.com>
2026-03-06 10:40:25 +0800

    fix: 修复ohos端

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Flutter\versions\stable"
+export "FLUTTER_ROOT=D:\Flutter\versions\custom_hm"
 export "FLUTTER_APPLICATION_PATH=D:\gohopo\jverify-flutter-plugin\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
